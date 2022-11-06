7de263533976e1a92279e2ce7b26f9661ff3efd5
varshanookarapu <lakshmivarsha.n@gmail.com>
2022-11-06 15:16:46 +0530

    Todo Application completed

---
+++ src/app/todo-card/todo-card.component.ts
@@ -0,0 +1,84 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ITask } from '../model/task';
+
+@Component({
+  selector: 'app-todo-card',
+  templateUrl: './todo-card.component.html',
+  styleUrls: ['./todo-card.component.scss'],
+})
+export class TodoCardComponent implements OnInit {
+  todoForm!: FormGroup;
+
+  tasks: ITask[] = [];
+  inprogress: ITask[] = [];
+  done: ITask[] = [];
+
+  updateIndex!: any;
+  isEditEnabled!: boolean;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item: ['', Validators.required],
+    });
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  addTask() {
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done: false,
+    });
+    this.todoForm.reset();
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  deleteInprogressTask(i: number) {
+    this.inprogress.splice(i, 1);
+  }
+
+  deleteDoneTask(i: number) {
+    this.done.splice(i, 1);
+  }
+
+  editTask(item: ITask, i: number) {
+    console.log(item, i);
+    this.todoForm.controls['item'].setValue(item.description);
+    this.updateIndex = i;
+    this.isEditEnabled = true;
+  }
+
+  updateTask() {
+    this.tasks[this.updateIndex].description = this.todoForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.todoForm.reset();
+    this.updateIndex = undefined;
+    this.isEditEnabled = false;
+    console.log(this.todoForm.value.item);
+  }
+}
